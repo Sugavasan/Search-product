8fde358539c7105769dfae5cbbc8b552767dab44
Sugavasan <140683482+Sugavasan@users.noreply.github.com>
2023-09-02 11:17:22 +0530

    Update Buyonline.js

--- Buyonline.js
+++ Buyonline.js
@@ -111,5 +111,23 @@ dark.addEventListener("click",()=>{
   past.classList.toggle('dark');
 });
 
-
+//contact
+function contact(){
+  var name = document.getElementById("username7").value;
+  var email = document.getElementById("email7").value;
+  var close = document.getElementById("reset7");
+  
+if(
+name === ""||
+email === ""
+){
+return false;
+}  if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)){
+alert("check your email")
+return false;
+}else{
+alert("Received your message successfully.")
+close.reset()
+}
+};
 
